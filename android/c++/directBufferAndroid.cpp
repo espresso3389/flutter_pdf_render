ba1551847e5cff7f61494f080afffd877d8178c7
Takashi Kawasaki <espresso3389@gmail.com>
2021-05-17 01:09:17 +0900

    Move directBufferAndroid.cpp.

--- android/c++/directBufferAndroid.cpp
+++ android/c++/directBufferAndroid.cpp
@@ -1,11 +1,9 @@
-#if defined(BUILD_FOR_ANDROID)
-
 #include <jni.h>
 #include <stdlib.h>
 
 extern "C" JNIEXPORT jobject JNICALL Java_jp_espresso3389_pdf_1render_ByteBufferHelper_newDirectBuffer(JNIEnv *env, jclass cls, jlong ptr, jlong size)
 {
-    return env->NewDirectByteBuffer(reinterpret_cast<void*>(static_cast<size_t>(ptr)), size);
+    return env->NewDirectByteBuffer(reinterpret_cast<void *>(static_cast<size_t>(ptr)), size);
 }
 
 extern "C" JNIEXPORT jlong JNICALL Java_jp_espresso3389_pdf_1render_ByteBufferHelper_malloc(JNIEnv *env, jclass cls, jlong size)
@@ -15,7 +13,5 @@ extern "C" JNIEXPORT jlong JNICALL Java_jp_espresso3389_pdf_1render_ByteBufferHe
 
 extern "C" JNIEXPORT void JNICALL Java_jp_espresso3389_pdf_1render_ByteBufferHelper_free(JNIEnv *env, jclass cls, jlong ptr)
 {
-    free(reinterpret_cast<void*>(static_cast<size_t>(ptr)));
+    free(reinterpret_cast<void *>(static_cast<size_t>(ptr)));
 }
-
-#endif
